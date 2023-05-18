b6d8695fa7acdc5f0f50a400e5b1e273183683fe
TopTopsEnric <enricguillotiblanco@gmail.com>
2023-05-18 18:57:52 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.render('pages/form')
+})
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`)
+})
